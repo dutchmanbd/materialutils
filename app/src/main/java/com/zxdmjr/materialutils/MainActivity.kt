4e36b8c6bde9b863a74be579ad6b0a92d1a7e184
Jewel Rana <jewel@ticonsys.com>
2019-09-16 11:02:25 +0600

    added sample code in project

--- app/src/main/java/com/zxdmjr/materialutils/MainActivity.kt
+++ app/src/main/java/com/zxdmjr/materialutils/MainActivity.kt
@@ -2,11 +2,68 @@ package com.zxdmjr.materialutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zxdmjr.material_utils.SharedPref
+import com.zxdmjr.material_utils.hide
+import com.zxdmjr.material_utils.show
+import com.zxdmjr.material_utils.toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val sharedPref by lazy {
+        SharedPref(applicationContext)
+    }
+
+    companion object{
+        private const val DATA_KEY = "data_key"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            toast("Hello Toast")
+        }
+
+        btnWriteData.setOnClickListener {
+            val data = etData.text.toString()
+
+            if(data.isNullOrEmpty()){
+                toast("Please enter data")
+                return@setOnClickListener
+            }
+            etData.setText("")
+            sharedPref.write(DATA_KEY, data)
+            toast("Write successfully")
+
+        }
+
+        btnReadData.setOnClickListener {
+            val data = sharedPref.read(DATA_KEY, "")
+
+            if(data.isNullOrEmpty()){
+                toast("No available data")
+                return@setOnClickListener
+            }
+            tvReadData.text = data
+        }
+
+        btnHideAll.setOnClickListener {
+            btnShowToast.hide()
+            btnWriteData.hide()
+            etData.hide()
+            btnReadData.hide()
+            tvReadData.hide()
+        }
+
+        btnShowAll.setOnClickListener {
+            btnShowToast.show()
+            btnWriteData.show()
+            etData.show()
+            btnReadData.show()
+            tvReadData.show()
+        }
+
     }
 }
