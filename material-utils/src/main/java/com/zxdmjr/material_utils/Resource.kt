0a1b1343bbeb1548eea96aebeb6b15b8ca55e26d
android-tms <jewel@ticonsys.com>
2020-11-18 20:43:51 +0600

    udpate error body

--- material-utils/src/main/java/com/zxdmjr/material_utils/Resource.kt
+++ material-utils/src/main/java/com/zxdmjr/material_utils/Resource.kt
@@ -1,7 +1,9 @@
 package com.zxdmjr.material_utils
 
+import okhttp3.ResponseBody
+
 sealed class Resource<out T> {
     data class Success<out T>(val data: T): Resource<T>()
     data class Loading(val nothing: Nothing? = null): Resource<Nothing>()
-    data class Failed(val message: String): Resource<Nothing>()
+    data class Failed(val errorBody: ResponseBody?, val message: String): Resource<Nothing>()
 }
--- material-utils/src/main/java/com/zxdmjr/material_utils/SafeApiRequest.kt
+++ material-utils/src/main/java/com/zxdmjr/material_utils/SafeApiRequest.kt
@@ -1,38 +1,20 @@
 package com.zxdmjr.material_utils
 
-import org.json.JSONException
-import org.json.JSONObject
 import retrofit2.Response
 
 abstract class SafeApiRequest {
 
-    suspend fun<T: Any> apiRequest(call: suspend () -> Response<T>) : Resource<T>{
+    suspend fun <T : Any> apiRequest(call: suspend () -> Response<T>): Resource<T> {
         val response = call.invoke()
-        if(response.isSuccessful){
-            if(response.body() == null){
-                return Resource.Failed("Unknown error occurred")
-            }else{
-                return Resource.Success(response.body()!!)
+        return if (response.isSuccessful) {
+            if (response.body() != null) {
+                Resource.Success(response.body()!!)
+            } else {
+                Resource.Failed(null, "Unknown error occurred")
             }
-        } else{
-            val error: String? = response.errorBody()?.string()
-            val message = StringBuilder()
-            error?.let{
-                try{
-                    val errorObject = JSONObject(it)
-                    if(errorObject.has("error")){
-                        val errorObject1 = errorObject.getJSONObject("error")
-                        for (key in errorObject1.keys()){
-                            message.append(errorObject1.getString(key)).append("\n")
-                        }
-                    } else{
-                        message.append("Unknown error occurred").append("\n")
-                    }
-                }catch(e: JSONException){
-                    message.append(e.message).append("\n")
-                }
-            }
-            return Resource.Failed(message.toString())
+        } else {
+            val errorBody = response.errorBody()
+            Resource.Failed(errorBody, "")
         }
     }
 }
